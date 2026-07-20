e81a13eb857ecb94782e608faf8995a4963e552b
Raul Tsintsadze <raultsintsadze@gmail.com>
2026-07-20 11:43:56 +0400

    add yandex metrica tracking

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -70,6 +70,13 @@ export default async function LocaleLayout({
         <Script id="ga4-init" strategy="afterInteractive">
           {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-SGF4Q9SSR7');`}
         </Script>
+        <Script id="yandex-metrica" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(110878720,"init",{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="https://mc.yandex.ru/watch/110878720" style={{position:'absolute',left:'-9999px'}} alt="" />
+        </noscript>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <FloatingButtonsProvider>
             <Header />
